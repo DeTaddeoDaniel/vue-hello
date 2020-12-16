6d06f9750fcdf324544f45758f95269cb1069c0f
Daniel2711 <detaddeo.d@gmail.com>
2020-12-16 19:44:00 +0100

    Esercizio stampa messaggio h1

---
+++ app.js
@@ -0,0 +1,10 @@
+// avvio Vue
+var app = new Vue({
+    
+    el: '#root', // div "radice" di vue
+    
+    data: { // contenitore di tutte le informazioni necessarie
+        messaggio: 'Ciao, sono un titolo da vue'
+    }
+    
+});
