c4a2e5e5d3ef94223bc1e80497096934f9076ece
Daniel2711 <detaddeo.d@gmail.com>
2020-12-16 19:49:24 +0100

    Bonus esercizio

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var app = new Vue({
     el: '#root', // div "radice" di vue
     
     data: { // contenitore di tutte le informazioni necessarie
-        messaggio: 'Ciao, sono un titolo da vue'
+        messaggio: 'Ciao, sono un titolo da vue',
+        urlImage: 'https://www.voglioviverecosi.com/wp-content/uploads/2019/05/VIVERE-A-CONTATTO-CON-LA-NATURA-1900x1080.jpg',
+        imageText: 'Sono  una foto'
     }
     
 });
