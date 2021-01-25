f3cf17c0eaf736eb5534ff5173a3021d946ae898
Daniel2711 <detaddeo.d@gmail.com>
2021-01-25 14:27:47 +0100

    Esercizio completato

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-// avvio Vue
+// Stampare a schermo un messaggio all’interno di 
+// un h1, utilizzando i data.
+
+
+// // Bonus: Aggiungere un’immagine presa anch’essa
+//  da un data assieme al valore alt dell'attributo.
+
+// Poi aggiungete all'immagine una classe sempre 
+// presa dal data.
+
 var app = new Vue({
     
-    el: '#root', // div "radice" di vue
+    el: '#root',
     
-    data: { // contenitore di tutte le informazioni necessarie
-        messaggio: 'Ciao, sono un titolo da vue',
+    data: {
+        title: 'Ciao, sono un titolo da vue',
         urlImage: 'https://www.voglioviverecosi.com/wp-content/uploads/2019/05/VIVERE-A-CONTATTO-CON-LA-NATURA-1900x1080.jpg',
-        imageText: 'Sono  una foto'
+        altImage: 'Sono  una foto',
+        classText: 'width100'
+    },
+
+    methods:{
+        classeDinamica(value){
+            console.log('change width to '+value)
+            this.classText= 'width'+value;
+        }
     }
     
 });
